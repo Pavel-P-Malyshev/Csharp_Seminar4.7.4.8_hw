076cd4e133aceab4c90d24be7a8fe19dcf3849f9
Pavel <m83819@gmail.com>
2022-09-08 03:09:54 +0700

    Task002_RangeSum

---
+++ Task2_RangeSum/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8 -> 30
+*/
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readValue=Console.ReadLine();
+    return int.Parse(readValue);
+}
+
+
+int RangeSum (int min, int max)
+{
+    if(min>max){
+        return 0;
+    } 
+    
+    return RangeSum(min, max-1)+max;
+    
+} 
+   
+
+
+int n=Prompt("enter N: ");
+int m=Prompt("enter m;");
+int result=RangeSum(n,m);
+Console.WriteLine($"sum is {result}");
